de9f5906de78fea0f89924c328bba30daa2b8c35
carlos muñoz <munoz.josecarlos@gmail.com>
2020-04-02 18:45:57 -0600

    Port as an environment variable and make an echo service

--- microservice.go
+++ microservice.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main(){
-	fmt.Print("Implement me!.\n")
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	return ":" + port
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hola Cloud Native Go")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, message)
 }
